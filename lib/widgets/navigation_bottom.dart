730fe9f433d71d9e0d0c90cd41709eba1c8f2237
Nejc <nejcpodlogar@gmail.com>
2025-04-20 17:31:31 +0200

    Computer change commit

--- lib/widgets/navigation_bottom.dart
+++ lib/widgets/navigation_bottom.dart
@@ -1,11 +1,8 @@
-// custom_bottom_navigation.dart
-
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:tabornski_sos_prirocnik_frontend/routing/route_definitions.dart';
 
 class CustomBottomNavigation extends StatefulWidget {
-
   CustomBottomNavigation({super.key});
 
   @override
@@ -14,33 +11,36 @@ class CustomBottomNavigation extends StatefulWidget {
 
 class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
   int _currentIndex = 0;
+  int _highLightedIndex = 0;
 
   void _onItemTapped(int index) {
+    int _index = index + 1;
 
-    if (_currentIndex != index) {
-      setState(() {
-        _currentIndex = index;
-      });
+    setState(() {
+      _highLightedIndex = index;
+      _currentIndex = _index;
+    });
 
-      switch (_currentIndex) {
-        case 1:
-          context.goNamed(RouteNames.morseCode);
-          break;
-        case 2:
-          context.goNamed(RouteNames.semaphore);
-          break;
-        case 3:
-          context.goNamed(RouteNames.orientation);
-          break;
-        case 4:
-          context.goNamed(RouteNames.settings);
-          break;
-        default:
-          context.goNamed(RouteNames.home);
-          break;
-      }
+    switch (index) {
+      case 0:
+        context.goNamed(RouteNames.morseCode);
+        break;
+      case 1:
+        context.goNamed(RouteNames.semaphore);
+        break;
+      case 2:
+        context.goNamed(RouteNames.orientation);
+        break;
+      case 3:
+        context.goNamed(RouteNames.settings);
+        break;
+      default:
+        break;
     }
 
+    if (_currentIndex != _index) {
+
+    }
   }
 
   @override
@@ -50,7 +50,7 @@ class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       showSelectedLabels: false,
       showUnselectedLabels: false,
-      currentIndex: _currentIndex,
+      currentIndex: _highLightedIndex,
       selectedIconTheme: IconThemeData(
         color: Theme.of(context).primaryColor,
       ),
@@ -58,27 +58,19 @@ class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
       onTap: _onItemTapped,
       items: const <BottomNavigationBarItem>[
         BottomNavigationBarItem(
-          icon: Icon(
-              Icons.more_horiz_sharp,
-          ),
-          label: ''
+          icon: Icon(Icons.more_horiz_sharp),
+          label: '',
         ),
         BottomNavigationBarItem(
-          icon: Icon(
-              Icons.flag_outlined,
-          ),
-          label: ''
+          icon: Icon(Icons.flag_outlined),
+          label: '',
         ),
         BottomNavigationBarItem(
-          icon: Icon(
-              Icons.map_outlined,
-          ),
-          label: ''
+          icon: Icon(Icons.map_outlined),
+          label: '',
         ),
         BottomNavigationBarItem(
-          icon: Icon(
-              Icons.settings_outlined,
-          ),
+          icon: Icon(Icons.settings_outlined),
           label: '',
         ),
       ],
